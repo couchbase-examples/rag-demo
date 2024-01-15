27de5103ffecb6894c5408f25434775304b51080
Nithish Raghunandanan <nithishr@users.noreply.github.com>
2024-01-15 20:46:22 +0100

    Add the initial RAG demo

---
+++ chat_with_pdf.py
@@ -0,0 +1,171 @@
+import tempfile
+from langchain_community.vectorstores import CouchbaseVectorStore
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+
+def save_to_vector_store(uploaded_file, vector_store):
+    """Chunk the PDF & store it in Couchbase Vector Store"""
+    if uploaded_file is not None:
+        temp_dir = tempfile.TemporaryDirectory()
+        temp_file_path = os.path.join(temp_dir.name, uploaded_file.name)
+
+        with open(temp_file_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
+            loader = PyPDFLoader(temp_file_path)
+            docs = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1500, chunk_overlap=150
+        )
+
+        doc_pages = text_splitter.split_documents(docs)
+
+        vector_store.add_documents(doc_pages)
+        st.info(f"PDF loaded into vector store in {len(doc_pages)} documents")
+
+
+@st.cache_resource(show_spinner="Connecting to Couchbase")
+def get_vector_store(
+    db_conn_str,
+    db_username,
+    db_password,
+    db_bucket,
+    db_scope,
+    db_collection,
+    _embedding,
+    index_name,
+):
+    """Return the Couchbase vector store"""
+    vector_store = CouchbaseVectorStore(
+        connection_string=db_conn_str,
+        db_username=db_username,
+        db_password=db_password,
+        bucket_name=db_bucket,
+        scope_name=db_scope,
+        collection_name=db_collection,
+        embedding=_embedding,
+        index_name=index_name,
+    )
+    return vector_store
+
+
+if __name__ == "__main__":
+    # Authorization
+    if "auth" not in st.session_state:
+        st.session_state.auth = False
+
+    AUTH = os.getenv("LOGIN_PASSWORD")
+    user_pwd = st.text_input("Enter password", type="password")
+    pwd_submit = st.button("Submit")
+
+    if pwd_submit and user_pwd == AUTH:
+        st.session_state.auth = True
+    elif pwd_submit and user_pwd != AUTH:
+        st.error("Incorrect password")
+
+    if st.session_state.auth:
+        # Load environment variables
+        DB_CONN_STR = os.getenv("DB_CONN_STR")
+        DB_USERNAME = os.getenv("DB_USERNAME")
+        DB_PASSWORD = os.getenv("DB_PASSWORD")
+        DB_BUCKET = os.getenv("DB_BUCKET")
+        DB_SCOPE = os.getenv("DB_SCOPE")
+        DB_COLLECTION = os.getenv("DB_COLLECTION")
+        INDEX_NAME = os.getenv("INDEX_NAME")
+
+        # Use OpenAI Embeddings
+        embedding = OpenAIEmbeddings()
+
+        vector_store = get_vector_store(
+            DB_CONN_STR,
+            DB_USERNAME,
+            DB_PASSWORD,
+            DB_BUCKET,
+            DB_SCOPE,
+            DB_COLLECTION,
+            embedding,
+            INDEX_NAME,
+        )
+
+        # Use couchbase vector store as a retriever for RAG
+        retriever = vector_store.as_retriever()
+
+        # Build the prompt for the RAG
+        template = """You are a helpful bot. Answer the question as truthfully as possible using the context below:
+        {context}
+
+        Question: {question}"""
+
+        prompt = ChatPromptTemplate.from_template(template)
+
+        # Use OpenAI GPT 4 as the LLM for the RAG
+        llm = ChatOpenAI(temperature=0, model="gpt-4-1106-preview", streaming=True)
+
+        # RAG chain
+        chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+
+        # Frontend
+        st.title("Chat with PDF")
+        with st.sidebar:
+            with st.form("upload pdf"):
+                uploaded_file = st.file_uploader(
+                    "Choose a PDF.",
+                    help="The document will be deleted after one hour of inactivity (TTL).",
+                    type="pdf",
+                )
+                submitted = st.form_submit_button("Upload")
+                if submitted:
+                    # store the PDF in the vector store after chunking
+                    save_to_vector_store(uploaded_file, vector_store)
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+            st.session_state.messages.append(
+                {
+                    "role": "assistant",
+                    "content": "Hi, I'm a chatbot who can chat with the PDF. How can I help you?",
+                }
+            )
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        # React to user input
+        if question := st.chat_input("Ask a question based on the PDF"):
+            # Display user message in chat message container
+            st.chat_message("user").markdown(question)
+
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": question})
+
+            # Add placeholder for streaming the response
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+
+            # stream the response from the RAG
+            full_response = ""
+            for chunk in chain.stream(question):
+                full_response += chunk
+                message_placeholder.markdown(full_response + "▌")
+
+            message_placeholder.markdown(full_response)
+            st.session_state.messages.append(
+                {"role": "assistant", "content": full_response}
+            )
+    # elif pwd_submit and user_pwd != AUTH:
+    #     st.error("Incorrect password")
